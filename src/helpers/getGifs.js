94e055c222f560a1a2797d57f8de470370a316d3
daadanielacosta <daniel.acosta@daikinapplied.com>
2021-02-16 12:05:20 -0600

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+const limit = '10';
+const apiKey = 'fUW991Du6i6HY5HWD2FBjo0MpzXWsst1';
+
+export const getGifs = async ( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=${limit}&api_key=${apiKey}`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return gifs;
+}
